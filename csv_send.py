88678f1ea6c82471287fd6a47aabb13424412554
KurodaTakeshi <57168394+KurodaTakeshi@users.noreply.github.com>
2020-01-17 12:23:31 +0900

    Update csv_send.py

--- csv_send.py
+++ csv_send.py
@@ -1,6 +1,5 @@
 import subprocess
-#cmd = r"scp -i ~/.ssh/id_rsa /home/pi/Desktop/data/kuroda_data.csv pi@35.189.138.28:/home/pi/data"
-cmd = r"scp -i ~/.ssh/id_rsa /home/pi/Desktop/data/My_data.csv pi@35.189.138.28:/home/pi/data"
+cmd = r"scp -i ~/.ssh/id_rsa /home/pi/Desktop/data/My_data.csv hoge@IPAddress:/home/pi/data"
 subprocess.call(cmd.split())
 
 import os
