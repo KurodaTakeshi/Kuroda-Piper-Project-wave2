fc87c8d825024d9673cc86faad8feb82d8f036c9
KurodaTakeshi <57168394+KurodaTakeshi@users.noreply.github.com>
2020-01-17 11:51:29 +0900

    add new file

--- csv_send.py
+++ csv_send.py
@@ -1,6 +1,8 @@
 import subprocess
-cmd = r"scp -i ~/.ssh/id_rsa /home/pi/Desktop/data/XXXXXX_data.csv pi@XX.XXX.XXX.XX:/home/pi/data"
+#cmd = r"scp -i ~/.ssh/id_rsa /home/pi/Desktop/data/kuroda_data.csv pi@35.189.138.28:/home/pi/data"
+cmd = r"scp -i ~/.ssh/id_rsa /home/pi/Desktop/data/My_data.csv pi@35.189.138.28:/home/pi/data"
 subprocess.call(cmd.split())
 
 import os
+os.system("python run_python_on_gcp.py")
 os.system("python3 popup.py")
