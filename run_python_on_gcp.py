60bdf51c56e056aeeaa90c8010e573619c0408c6
KurodaTakeshi <57168394+KurodaTakeshi@users.noreply.github.com>
2020-01-17 12:06:03 +0900

    add new file

--- run_python_on_gcp.py
+++ run_python_on_gcp.py
@@ -1,3 +1,4 @@
 import os
 
-os.system("ssh pi@XX.XXX.XXX.XX -i ~/.ssh/id_rsa python read_and_insert.py")
+os.system("ssh pi@35.189.138.28 -i ~/.ssh/id_rsa python read_and_insert.py")
+os.system("python notification_webhook.py")
